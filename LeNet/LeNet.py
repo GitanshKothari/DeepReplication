3212bd5bfc63865a29877f1039a27cc9f982807b
Gitanshk <gitanshkothari@gmail.com>
2023-11-01 20:03:20 -0400

    updated code comments

--- LeNet/LeNet.py
+++ LeNet/LeNet.py
@@ -2,6 +2,14 @@ import torch
 import torch.nn as nn
 from torchsummary import summary
 
+"""
+    This code defines the LeNet architecture for image classification. 
+    It consists of two convolutional layers, followed by three fully connected layers. 
+    The ReLU activation function is used throughout the network. 
+    The forward method defines the flow of data through the network. 
+    The input image is first passed through the convolutional layers, then the output is flattened and passed through the fully connected layers. 
+    Finally, the output layer produces the classification scores.
+"""
 class LeNet(nn.Module):
     def __init__(self):
         """
